d53bd8b623b2d1392b696591ec48fdd461c53ecd
Kishore Kumar v <kishore.kumarkv93@gmail.com>
2024-05-04 14:27:33 +0530

    Database added and connectde

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Kishore",
+    author_email="kishore.kumarkv93@gmail.com",
+    packages=find_packages()
+)
